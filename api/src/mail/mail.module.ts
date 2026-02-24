3304b762815aeea8328399baa4c8d71742bb04fb
Chouaib <chouaib.skt@gmail.com>
2026-02-24 10:24:09 +0100

    feat: enhance MailModule with additional email validation providers

--- api/src/mail/mail.module.ts
+++ api/src/mail/mail.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MailService } from './mail.service';
+import { EmailValidationService } from './email-validation.service';
+import { ZeroBounceProvider } from './providers/zerobounce.provider';
+import { DeepValidatorProvider } from './providers/deep-validator.provider';
 import mailConfig from '../config/mail.config';
+import emailValidationConfig from '../config/email-validation.config';
 
 @Module({
-  imports: [ConfigModule.forFeature(mailConfig)],
-  providers: [MailService],
-  exports: [MailService],
+  imports: [
+    ConfigModule.forFeature(mailConfig),
+    ConfigModule.forFeature(emailValidationConfig),
+  ],
+  providers: [
+    MailService,
+    EmailValidationService,
+    ZeroBounceProvider,
+    DeepValidatorProvider,
+  ],
+  exports: [MailService, EmailValidationService],
 })
 export class MailModule {}
