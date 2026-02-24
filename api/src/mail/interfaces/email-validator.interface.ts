05ddd0f7faf6df7f07e12583b7282c334934c7be
Chouaib <chouaib.skt@gmail.com>
2026-02-24 10:24:13 +0100

    fix: remove trailing whitespace in EmailValidationResult interface

--- api/src/mail/interfaces/email-validator.interface.ts
+++ api/src/mail/interfaces/email-validator.interface.ts
@@ -1,7 +1,7 @@
 export interface EmailValidationResult {
   valid: boolean;
   reason?: string;
-  suggestion?: string; 
+  suggestion?: string;
   isDisposable?: boolean;
   isCatchAll?: boolean;
   score?: number; // Quality score 0-100
