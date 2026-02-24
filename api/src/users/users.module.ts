72b3d82be895c48501405a8cb8d15d1c7672cdda
Chouaib <chouaib.skt@gmail.com>
2026-02-24 10:24:22 +0100

    feat: add MailModule import to UsersModule to resolve circular dependency

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -5,11 +5,13 @@ import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { Role } from '../roles/entities/role.entity';
 import { AuthModule } from '../auth/auth.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Role]),
     forwardRef(() => AuthModule), // ← Add this (forwardRef to avoid circular dependency)
+    MailModule,
   ],
   controllers: [UsersController],
   providers: [UsersService],
